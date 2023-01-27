842cd45b529e3a844fe0d3136c28362d3e455e0e
Samory Sundjata <samory.sundjata@gmail.com>
2023-01-27 14:26:43 -0300

    NacionalidadesController

---
+++ src/Jukebox.API/Controllers/NacionalidadesController.cs
@@ -0,0 +1,43 @@
+﻿using Jukebox.API.Context;
+using Jukebox.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jukebox.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class NacionalidadesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public NacionalidadesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public ActionResult<IEnumerable<Nacionalidade>> GetNacionalidades() 
+        { 
+            var nacionalidades = _context.Nacionalidades.ToList();
+
+            if (nacionalidades is null)
+            {
+                return NotFound("Não há nacionalidades");
+            }
+
+            return nacionalidades;
+
+        }
+        
+        public ActionResult<Nacionalidade> GetNacionalidade(string sigla)
+        {
+            var nacionalidade  = _context.Nacionalidades.FirstOrDefault(n => n.Sigla == sigla);
+
+            if (nacionalidade is null)
+            {
+                return NotFound("Nacionalidade não encontrada.");
+            }
+
+            return nacionalidade;
+        }
+    }
+}
