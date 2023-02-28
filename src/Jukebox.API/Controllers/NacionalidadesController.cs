078d2b41594c16198f42fd832c5999e22888e351
Samory Sundjata <samory.sundjata@gmail.com>
2023-02-28 14:54:57 -0300

    Correções em arquivos e nacionalidades com trycatch

--- src/Jukebox.API/Controllers/NacionalidadesController.cs
+++ src/Jukebox.API/Controllers/NacionalidadesController.cs
@@ -18,55 +18,100 @@ namespace Jukebox.API.Controllers
 
         [HttpGet]
         public ActionResult<IEnumerable<Nacionalidade>> GetNacionalidades() 
-        { 
-            var nacionalidades = _context.Nacionalidades.ToList();
-            if (nacionalidades is null)  { return NotFound("Não há nacionalidades cadastradas."); }
-            return nacionalidades;
+        {
+            try
+            {
+                var nacionalidades = _context.Nacionalidades.ToList();
+                if (nacionalidades is null)  { return NotFound("Não há nacionalidades cadastradas."); }
+                return nacionalidades;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+           
         }
 
         [HttpGet("{sigla:string}", Name = "TrazerNacionalidade")]
         public ActionResult<Nacionalidade> GetNacionalidade(string sigla)
         {
-            var nacionalidade  = _context.Nacionalidades.FirstOrDefault(n => n.Sigla == sigla);
-            if (nacionalidade is null)  { return NotFound("Nacionalidade não encontrada."); }
-            return nacionalidade;
+            try
+            {
+                var nacionalidade  = _context.Nacionalidades.FirstOrDefault(n => n.Sigla == sigla);
+                if (nacionalidade is null)  { return NotFound("Nacionalidade não encontrada."); }
+                return nacionalidade;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
         }
 
         [HttpPost]
         public ActionResult PostNacionalidade(Nacionalidade nacionalidade)
         {
-            //TODO: verificar se a sigla já existe antes de inserir
-            //Fazer um método de busca pelo nome.
-            if(nacionalidade is null) { return BadRequest(); }
-            _context.Nacionalidades.Add(nacionalidade);
-            _context.SaveChanges();
+            try
+            {
+                //TODO: verificar se a sigla já existe antes de inserir
+                //Fazer um método de busca pelo nome.
+                if(nacionalidade is null) { return BadRequest(); }
+                _context.Nacionalidades.Add(nacionalidade);
+                _context.SaveChanges();
 
-            return new CreatedAtRouteResult("TrazerNacionalidade", 
-                new { sigla = nacionalidade.Sigla }, nacionalidade);
+                return new CreatedAtRouteResult("TrazerNacionalidade", 
+                    new { sigla = nacionalidade.Sigla }, nacionalidade);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
         }
 
         [HttpPut("sigla:string")]
         public ActionResult PutNacionalidade(string sigla, Nacionalidade nacionalidade)
         {
-            if (sigla != nacionalidade.Sigla) { return BadRequest(); }        
+            try
+            {
+                if (sigla != nacionalidade.Sigla) { return BadRequest(); }        
             
-            _context.Entry(nacionalidade).State = EntityState.Modified;
-            _context.SaveChanges();
+                _context.Entry(nacionalidade).State = EntityState.Modified;
+                _context.SaveChanges();
 
-            return Ok(nacionalidade);
+                return Ok(nacionalidade);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
         }
 
         [HttpDelete]
         public ActionResult DeleteNacionalidade(string sigla)
         {
-            var nacionalidade = _context.Nacionalidades.FirstOrDefault(n => n.Sigla == sigla);
+            try
+            {
+                var nacionalidade = _context.Nacionalidades.FirstOrDefault(n => n.Sigla == sigla);
 
-            if(nacionalidade is null) { return NotFound("Nacionalidade não encontrada para exclusão."); }
+                if(nacionalidade is null) { return NotFound("Nacionalidade não encontrada para exclusão."); }
 
-            _context.Nacionalidades.Remove(nacionalidade);
-            _context.SaveChanges();
+                _context.Nacionalidades.Remove(nacionalidade);
+                _context.SaveChanges();
 
-            return Ok(nacionalidade);
+                return Ok(nacionalidade);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
         }
 
     }
